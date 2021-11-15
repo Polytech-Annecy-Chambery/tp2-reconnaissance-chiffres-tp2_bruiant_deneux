b124938130227749d2a57e4a6100f201f1e880f1
basdx <93935383+basdx@users.noreply.github.com>
2021-11-15 17:26:25 +0100

    Update reconnaissance.py

--- src/reconnaissance.py
+++ src/reconnaissance.py
@@ -12,5 +12,19 @@ def lecture_modeles(chemin_dossier):
 
 
 def reconnaissance_chiffre(image, liste_modeles, S):
-    pass
-
+    im = image.binarisation(S)
+    im = im.localisation()
+    liste_similitude = []
+    for i in range (len(liste_modeles)) :
+        im = im.resize(liste_modeles[i].H, liste_modeles[i].W)
+        reco = im.similitude(liste_modeles[i])
+        liste_similitude.append(reco)
+        
+    maxListe= 0
+    max_simil = liste_similitude[0]
+    for j in range (1,len(liste_similitude)):
+        if liste_similitude[j] > max_simil :
+            maxListe= j
+            max_simil = liste_similitude[j]
+            
+    return maxListe
